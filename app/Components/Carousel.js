d1d41c8f5336b187bf79a7c6ad4b65602ad32fca
lyctea <396136337@qq.com>
2017-04-05 21:53:02 +0800

    配置redux-devtool

--- app/Components/Carousel.js
+++ app/Components/Carousel.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import Carousel from 'antd/lib/carousel'
 import 'antd/lib/carousel/style/index.less'
+import { Link } from 'react-router-dom'
+
 
 export default class MyCarousel extends Component{
     render(){
         const data = [
             {
                 img: require("../image/banner1.jpg"),
-                link: 'lessonDetail'
+                link: 'category'
             },
             {
                 img: require("../image/banner2.jpg"),
@@ -29,7 +31,7 @@ export default class MyCarousel extends Component{
         return (
             <Carousel autoplay dots infinite selectedIndex={1} className="xxx" style={{ backgroundColor: 'blue', margin: 10 }}>
                 {data.map((d, i) => {
-                    return <a href={d.link} key={i}><img style={{ verticalAlign: 'top',margin: "0 auto",width: "100%"}} src={d.img}/></a>;
+                    return <Link to="category" key={i}><img style={{ verticalAlign: 'top',margin: "0 auto",width: "100%"}} src={d.img}/></Link>;
                 })}
             </Carousel>
         )
--- app/Reducer/store.js
+++ app/Reducer/store.js
@@ -1,16 +1,28 @@
-//连接中间件 用于生成项目的唯一store
-import { createStore, applyMiddleware } from 'redux'
-import * as reducers from '../Reducer/reducers';
+import { createStore,combineReducers,applyMiddleware,compose } from 'redux'
+import * as reducers from '../Reducer/reducers'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 
-const loggerMiddleware = createLogger()
+let createStoreWithMiddleware = null
 
-const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-)(createStore)
-
-export default function configureStore(initiaState) {
-    return createStoreWithMiddleware(reducers,initiaState)
+if (process.env.NODE_ENV === 'production'){
+    createStoreWithMiddleware = compose(applyMiddleware(
+        thunkMiddleware
+    ))(createStore)
+}else {
+    createStoreWithMiddleware = compose(applyMiddleware(
+        thunkMiddleware,
+        createLogger
+    ),window.devToolsExtension?window.devToolsExtension():f=>f)(createStore)
 }
+/*
+* compose
+* (arguments): 需要合成的多个函数。每个函数都接收一个函数作为参数，然后返回一个函数。
+* (return ): 从右到左把接收到的函数合成后的最终函数。
+* */
+
+var store = createStoreWithMiddleware(
+    combineReducers(reducers),
+);
+
+export default store;
